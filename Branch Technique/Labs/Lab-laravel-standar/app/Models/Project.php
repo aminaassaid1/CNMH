6747b2b8a0cd695e1ff2510ae306cc3f7c84b966
Amina Assaid <aminaassaid123@gmail.com>
2023-12-29 13:03:12 +0100

    Add

--- Branch Technique/Labs/Lab-laravel-standar/app/Models/Project.php
+++ Branch Technique/Labs/Lab-laravel-standar/app/Models/Project.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nom',
+        'description',
+    ];
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- Branch Technique/Labs/Lab-laravel-standar/app/Models/Task.php
+++ Branch Technique/Labs/Lab-laravel-standar/app/Models/Task.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nom',
+        'description',
+        'projetId',
+    ];
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
